7e5235930079cd2f6157fb76cf2ff5ed3b79c020
TheMartonfi <carlmartonfi@hotmail.com>
2020-12-16 20:41:37 -0700

    Make graphql schema and add it to /graphql end point

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({ graphiql: true }));
+app.use("/graphql", graphqlHTTP({ graphiql: true, schema }));
 
 app.listen(3000, () => console.log("Server listening on port 3000!"));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,35 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt } = graphql;
+const {
+	GraphQLObjectType,
+	GraphQLID,
+	GraphQLString,
+	GraphQLInt,
+	GraphQLSchema
+} = graphql;
 
 const UserType = new GraphQLObjectType({
 	name: "User",
 	description: "Documentaion for user...",
 	fields: () => ({
-		id: { type: GraphQLID },
+		id: { type: GraphQLString },
 		name: { type: GraphQLString },
 		age: { type: GraphQLInt }
 	})
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: "RootQueryType",
+	description: "Description",
+	fields: {
+		user: {
+			type: UserType,
+			args: { id: { type: GraphQLString } },
+			resolve(parent, args) {}
+		}
+	}
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
 });
