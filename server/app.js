0a88f6a709f22642010c41734f291264136dea84
TheMartonfi <carlmartonfi@hotmail.com>
2020-12-16 19:43:48 -0700

    Add graphql route with graphiql

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP({ graphiql: true }));
 
 app.listen(3000, () => console.log("Server listening on port 3000!"));
