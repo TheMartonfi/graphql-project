098e7517fb86a41bd12a50a471c5eff8199370a7
TheMartonfi <carlmartonfi@hotmail.com>
2020-12-30 16:58:53 -0700

    Add dummy data and make root query return user by id

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,16 @@
 const graphql = require("graphql");
+const _ = require("lodash");
+
+// dummy data
+const usersData = [
+	{ id: "1", name: "Bond", age: 36, country: "Japan" },
+	{ id: "13", name: "Anna", age: 26, country: "Canada" },
+	{ id: "211", name: "Bella", age: 16, country: "United States" },
+	{ id: "19", name: "Gina", age: 26, country: "South Korea" },
+	{ id: "150", name: "Georgina", age: 36, country: "France" },
+	{ id: "293", name: "Bob", age: 48, country: "Germany" },
+	{ id: "392", name: "Timmy", age: 6, country: "Taco Bell" }
+];
 
 const {
 	GraphQLObjectType,
@@ -14,7 +26,8 @@ const UserType = new GraphQLObjectType({
 	fields: () => ({
 		id: { type: GraphQLString },
 		name: { type: GraphQLString },
-		age: { type: GraphQLInt }
+		age: { type: GraphQLInt },
+		country: { type: GraphQLString }
 	})
 });
 
@@ -25,7 +38,10 @@ const RootQuery = new GraphQLObjectType({
 		user: {
 			type: UserType,
 			args: { id: { type: GraphQLString } },
-			resolve(parent, args) {}
+			resolve(parent, args) {
+				// return _.find(usersData, { id: args.id });
+				return usersData.find((user) => user.id === args.id);
+			}
 		}
 	}
 });
